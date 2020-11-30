b80ec6e5a21f598f04d1438c452605095eb5e970
Sanjay Toge <65099538+t-sanjay@users.noreply.github.com>
2020-11-30 16:59:28 +0530

    http to https

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showPosition(position){
 }
 
 async function sendApirequest(position){
-    var response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=050ef660cb3a26caa60021ed823bac6b`);
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=050ef660cb3a26caa60021ed823bac6b`);
     var data = await response.json();
     var y = document.getElementById("okay"); 
     y.innerHTML = " data = " + `${data.main.temp}`;
